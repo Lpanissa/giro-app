1cf6cf353ef1dbacf8e59205e9678fc04610a605
Lucas Panissa <101229696+Lpanissa@users.noreply.github.com>
2026-09-07 12:50:14 -0300

    Update ProfitPage.tsx

--- giro-app-main/src/pages/ProfitPage.tsx
+++ giro-app-main/src/pages/ProfitPage.tsx
@@ -263,7 +263,7 @@ export function ProfitPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center text-center py-2">
+    <div className="spac-y5 overflow-y-auto pt-2 pb-16 pr-1">
     </div>
   <h1 className="text-2xl font-bold text-slate-900">Vendas & Lucro</h1>
   <p className="text-sm text-slate-500">Acompanhe o desempenho diário</p>
